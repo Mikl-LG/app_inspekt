82a1af81f9e54feefcad33dd3cfc8546cac2cee3
Mikl-LG <legallicmickael@inspekt.fr>
2020-04-28 07:50:21 +0200

    initiation

---
+++ src/home.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+
+class Home extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={}
+    }
+
+    render(){
+
+        return(
+            <p>Hello world !</p>
+        )
+    }
+
+}
+
+export default Home;
